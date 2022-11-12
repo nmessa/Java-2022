ff2562f73f47d1a09175f8d9221e1ad8eb64acf9
nmessa <nmessa@sau16.org>
2022-11-12 09:42:58 -0500

    Add files via upload

---
+++ Lab Exercise 11.16.2022/BigBucks/src/BankAccount.java
@@ -0,0 +1,31 @@
+/**
+ * @(#)BankAccount.java
+ *
+ *
+ * @Author: 
+ * @version 1.00 2022/11/16
+ */
+
+
+public class BankAccount {
+	public String name;
+	public double balance;
+
+    public BankAccount(String nm, double amt) 
+    {
+    	//Add code here
+    	
+    }
+    
+    public void deposit(double dp)
+    {
+    	//Add code here
+    	
+    }
+    
+    public void withdraw(double wd)
+    {
+    	//Add code here
+    	
+    }
+}
---
+++ Lab Exercise 11.17.2022/ComplexNumber/src/Complex.java
@@ -0,0 +1,11 @@
+//Complex class definition
+//Author: 
+//Date: 11/17/2022
+//This program implements a complex number class
+
+public class Complex 
+{
+   	//Add code here
+   	
+   	
+}
