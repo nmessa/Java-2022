c74f0184789485b5935f03259fbf9b9c72c2cbd9
nmessa <nmessa@sau16.org>
2023-01-08 11:04:14 -0500

    Add files via upload

--- Lab Exercise 1.11.2023/Problem 2/src/Problem_2.java
+++ Lab Exercise 1.11.2023/Problem 2/src/Problem_2.java
@@ -3,8 +3,8 @@
  *
  * Problem_2 application
  *
- * @Author: nmessa 
- * @version 1.00 2023/1/11
+ * @author 
+ * @version 1.00 2023/1/8
  */
  
 public class Problem_2 {
@@ -12,19 +12,23 @@ public class Problem_2 {
     public static void main(String[] args) {
     	
     	int[] array1 = {50, -20, 0, 30, 40, 60, 12};
-    	int[] array2 = {45, 20, 10, 20, 30, 50, 12};
-    	int[] array3 = {25, -20, 0, 30, 40, 60, 12};
-    	int[] array4 = {45, 20, 10, 20, 30, 50, 11};
+    	int[] array2 = {45, 20, 10, 20, 300, 50, 11};
+    	int[] array3 = {50, 20, 10, 20, 300, 50, 11};
+    	int[] array4 = {45, 20, 10, 20, 300, 50, 12};
+    	
     	System.out.println(same(array1, array2));
-    	System.out.println(same(array2, array4));
+    	System.out.println(same(array1, array3));
     	System.out.println(same(array1, array4));
     }
     
     static boolean same(int[] a1, int[] a2)
     {
-    	if (a1[0] == a2[0] || a1[a1.length - 1] == a2[a2.length - 1])
-    		return true;
-    	else
-    		return false;
+    	//add code here
+    	
     }
 }
+
+//Output
+//false
+//true
+//true
